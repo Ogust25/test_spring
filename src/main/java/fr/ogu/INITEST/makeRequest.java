de35cc44a430511b0c0d88e1f529ef454a05fdb1
August Gros <august.gros@gmail.com>
2023-03-08 16:33:38 +0100

    oui

---
+++ src/main/java/fr/ogu/INITEST/makeRequest.java
@@ -0,0 +1,31 @@
+package fr.ogu.INITEST;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class makeRequest {
+    public static void main(String[] args) {
+        checkUrl();
+    }
+
+    private static String URL = "https://emailvalidation.abstractapi.com/v1/?api_key=028ad2af6f84427a8fd2c3e28d1a2256&email=";
+
+    public static void checkUrl() {
+        HttpGet req = new HttpGet(URL + "august.gros@gmail.com");
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        try {
+            CloseableHttpResponse execute = httpClient.execute(req);
+            BufferedReader is = new BufferedReader(new InputStreamReader(execute.getEntity().getContent()));
+            System.out.println(is.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
